587f269b030df2bd8d02737a27f2ee5251cd0878
shrvn12 <singhshravan1208@gmail.com>
2026-06-07 15:33:13 +0530

    Add plugin: added PWA plugin

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,39 @@
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// export default defineConfig({
+//   plugins: [vue()],
+// })
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+    vue(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      includeAssets: [
+        'favicon.ico'
+      ],
+
+      manifest: {
+        name: 'OCR System',
+        short_name: 'OCR',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff'
+      },
+
+      workbox: {
+        globPatterns: [
+          '**/*.{js,css,html,png,svg,ico}'
+        ]
+      }
+    })
+  ]
 })
